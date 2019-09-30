0647543964170b5461956156b4bc682c52395e09
sunmo <sunmo@yunfutech.com>
2019-09-30 14:14:06 -0400

    fix test us04

--- project03/test.py
+++ project03/test.py
@@ -1,5 +1,6 @@
 import unittest
 import iden
+import user_story
 
 path = 'project03.ged'
 iden_list = iden.read_ind_info(path)
@@ -8,14 +9,14 @@ fam_list = iden.read_fam_info(path)
 
 class TestUserStory(unittest.TestCase):
     def test_us04_01(self):
-        result = iden.us_04(fam_list)
+        result = user_story.us_04(fam_list)
         self.assertEqual(result, ['F2'])
 
     def test_us04_02(self):
         test_list = []
         test_list.append(iden.Family('F1', '1997-1-3', '2020-3-1', None, None, None, None, None))
         test_list.append(iden.Family('F2', '1000-1-3', '2020-3-1', None, None, None, None, None))
-        result = iden.us_04(test_list)
+        result = user_story.us_04(test_list)
         self.assertEqual(result, [])
 
     def test_us04_03(self):
@@ -23,7 +24,7 @@ class TestUserStory(unittest.TestCase):
         test_list.append(iden.Family('F1', '1997-1-3', '1973-3-1', None, None, None, None, None))
         test_list.append(iden.Family('F2', '1000-1-3', '1000-3-1', None, None, None, None, None))
         test_list.append(iden.Family('F3', '1000-1-3', '1000-1-1', None, None, None, None, None))
-        result = iden.us_04(test_list)
+        result = user_story.us_04(test_list)
         self.assertEqual(result, ['F1', 'F3'])
 
 
