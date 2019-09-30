e4bb0e1ff054d9d33cf538f290eb31886d21a64b
徐一鸣 <xuyiming@MacBook.local>
2019-09-30 15:45:24 -0400

    user-story-8

--- project03/user_story.py
+++ project03/user_story.py
@@ -7,6 +7,7 @@ import unittest
 # Author Mo Sun
 def us_04(fam_list):
     res = []
+    res_error= []
     for fam in fam_list:
         married = fam.married
         divorced = fam.divorced
@@ -23,12 +24,14 @@ def us_04(fam_list):
             if wrong:
                 print("ERROR: FAMILY: US04: " + fam.id + ": Divorced " + fam.divorced + " before married " + fam.married)
                 res.append(fam.id)
-    return res
+                res_error.append("ERROR: FAMILY: US04: " + fam.id + ": Divorced " + fam.divorced + " before married " + fam.married)
+    return res,res_error
 
 # Author Yiming Xu
 def user_story_5(test_ind_list,test_fam_list):
     s1 = 'Marriage before death'
     res = []
+    res_error = []
     for i in test_ind_list:
         id = i.id
         death = i.death
@@ -44,52 +47,25 @@ def user_story_5(test_ind_list,test_fam_list):
                 res1 = 'Marriage before death'
             if item.husband_id == id and item.married > death:
                 print("ERROR: FAMILY: US05: %s: Marriage %s after Husband's death %s" %(item.id,item.married,death))
-                res1 = "ERROR: FAMILY: US05: %s: Marriage %s after Husband's death %s" % (item.id, item.married, death)
+                res0 = "ERROR: FAMILY: US05: %s: Marriage %s after Husband's death %s" % (item.id, item.married, death)
                 res.append(item.husband_id)
+                res_error.append(res0)
             if item.wife_id == id and item.married < death:
                 print(s1)
 
             if item.wife_id == id and item.married > death:
                 print("ERROR: FAMILY: US05: %s: Marriage %s after Wife's death %s" %(item.id,item.married,death))
-                res1 = "ERROR: FAMILY: US05: %s: Marriage %s after Wife's death %s" %(item.id,item.married,death)
+                res0 = "ERROR: FAMILY: US05: %s: Marriage %s after Wife's death %s" %(item.id,item.married,death)
                 res.append(s1)
-    return res
-
-def user_story_07(test_ind_list):
-    res = []
-    for i in test_ind_list:
-        id = i.id
-        if i.age > 150:
-            print("ERROR: INDIVIDUAL: US07: %s: More than 150 years old - Birth date %s " %(id,i.birthday))
-            res1 = "ERROR: INDIVIDUAL: US07: %s: More than 150 years old - Birth date %s " %(id,i.birthday)
-            res.append(id)
-        if i.age<150:
-            print('Less than 150 years old')
-            res1 = 'Less than 150 years old'
-    return res
-
-# Author Yiming Xu
-def user_story_08(test_ind_list,test_fam_list):
-    res = []
-    for i in test_ind_list:
-        id = i.id
-        birth = i.birthday
-        for item in test_fam_list:
-            if id in item.children and item.married > birth:
-                print('ANOMALY: FAMILY: US08: %s: Child %s born before marriage on %s' %(item.ID,id,item.married))
-                res1 = 'ANOMALY: FAMILY: US08: %s: Child %s born before marriage on %s' %(item.ID,id,item.married)
-                res.append(item.id)
-            if id in item.children and item.married < birth:
-                print('Birth before marriage of parents')
-    return res
-
-# user_story_08(ind_list,fam_list)
+                res_error.append(res0)
+    return res,res_error
 
 ## Author Yiming Xu and Mo Sun
 ## Pair Programming
 def user_story_6(test_ind_list,test_fam_list):
     s1 = 'Divorce before death'
     res = []
+    res_error=[]
     for i in test_ind_list:
         id = i.id
         death = i.death
@@ -104,16 +80,61 @@ def user_story_6(test_ind_list,test_fam_list):
                 print("ERROR: FAMILY: US05: %s: Divorce %s after Husband's death %s" %(item.id,item.divorce,death))
                 res2 = "ERROR: FAMILY: US05: %s: Divorce %s after Husband's death %s" % (item.id, item.divorce, death)
                 res.append(item.husband_id)
+                res_error.append(res2)
             if item.wife_id == id and item.divorced < death:
                 print(s1)
             if item.wife_id == id and item.divorced > death:
                 print("ERROR: FAMILY: US05: %s: Divorce %s after Wife's death %s" %(item.id,item.divorced,death))
                 res4 = "ERROR: FAMILY: US05: %s: Divorce %s after Wife's death %s" %(item.id,item.divorced,death)
                 res.append(item.wife_id)
+                res_error.append(res4)
     return res
+
+## Author Yiming Xu
+def user_story_07(test_ind_list):
+    res = []
+    res_error = []
+    for i in test_ind_list:
+        id = i.id
+        if i.age > 150:
+            print("ERROR: INDIVIDUAL: US07: %s: More than 150 years old - Birth date %s " %(id,i.birthday))
+            res1 = "ERROR: INDIVIDUAL: US07: %s: More than 150 years old - Birth date %s " %(id,i.birthday)
+            res.append(id)
+            res_error.append(res1)
+        if i.age<150:
+            print('Less than 150 years old')
+            res1 = 'Less than 150 years old'
+    return res,res_error
+
+# Author Yiming Xu
+def user_story_08(test_ind_list,test_fam_list):
+    res = []
+    res_error = []
+    for i in test_ind_list:
+        id = i.id
+        birth = i.birthday
+        for item in test_fam_list:
+            if id in item.children and item.married > birth:
+                print('ANOMALY: FAMILY: US08: %s: Child %s born before marriage on %s' %(item.ID,id,item.married))
+                res0 = 'ANOMALY: FAMILY: US08: %s: Child %s born before marriage on %s' %(item.ID,id,item.married)
+                res_error.append(res0)
+                res.append(item.id)
+            if id in item.children and item.married < birth:
+                print('Birth before marriage of parents')
+    return res,res_error
+
+# user_story_08(ind_list,fam_list)
+
+
 
 if __name__ == '__main__':
     ind_list = iden.read_ind_info('project03.ged')
-
+    ind_table = iden.create_ind_table(ind_list)
     fam_list = iden.read_fam_info('project03.ged')
-    print(user_story_5(ind_list,fam_list))
+    fam_table = iden.creat_fam_table(fam_list)
+
+    # print(user_story_5(ind_list,fam_list))
+    # with open('output.txt', 'a') as file:
+    #     file.write(ind_table)
+    #     file.write(fam_table)
+    #     file.write()
