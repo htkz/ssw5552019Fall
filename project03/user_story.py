c5e94dfeb9347d2161623cd4d44df4407f24ee19
sunmo <sunmo@yunfutech.com>
2019-09-30 14:03:37 -0400

    Added user story 4

--- project03/user_story.py
+++ project03/user_story.py
@@ -5,6 +5,29 @@ ind_list = iden.read_ind_info('project03.ged')
 fam_list = iden.read_fam_info('project03.ged')
 
 
+# return list of family id that divorced before married
+# Author Mo Sun
+def us_04(fam_list):
+    res = []
+    for fam in fam_list:
+        married = fam.married
+        divorced = fam.divorced
+        if divorced != 'NA':
+            married = married.split('-')
+            divorced = divorced.split('-')
+            wrong = False
+            for i in range(3):
+                if int(married[i]) < int(divorced[i]):
+                    break
+                if int(married[i]) > int(divorced[i]):
+                    wrong = True
+                    break
+            if wrong:
+                print("ERROR: FAMILY: US04: " + fam.id + ": Divorced " + fam.divorced + " before married " + fam.married)
+                res.append(fam.id)
+    return res
+
+
 def user_story_5(test_ind_list,test_fam_list):
     for i in test_ind_list:
         id = i.id
