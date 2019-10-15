c589e73d029cacdf43329634ad5c3d79221d4680
sunmo <sunmo@yunfutech.com>
2019-10-14 20:26:07 -0400

    Achieved one line application

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # SSW 555 2019Fall Group Project
+
+### Get started
+
+Run the script ```python3 index.py``` in the directory of index.py to see the result of our program.
+
---
+++ project/index.py
@@ -0,0 +1,46 @@
+from iden import *
+from user_story import *
+
+
+if __name__ == '__main__':
+    ind_list = read_ind_info('project03.ged')
+    ind_table = create_ind_table(ind_list)
+    print(ind_table)
+
+    fam_list = read_fam_info('project03.ged')
+    fam_table = creat_fam_table(fam_list)
+    print(fam_table)
+
+    ind_list = iden.read_ind_info('project03.ged')
+    ind_table = iden.create_ind_table(ind_list)
+    fam_list = iden.read_fam_info('project03.ged')
+    fam_table = iden.creat_fam_table(fam_list)
+
+    with open('output.txt', 'w') as file:
+        file.write(str(ind_table))
+        file.write('\n')
+        file.write(str(fam_table))
+        file.write('\n')
+
+
+        def writedowm(res_error):
+            for i in res_error:
+                file.write(str(i) + '\n')
+
+
+        writedowm(user_story_01(ind_list, fam_list)[1])
+        writedowm(us_02(ind_list, fam_list)[1])
+        writedowm(us_03(ind_list)[1])
+        writedowm(us_04(fam_list)[1])
+        writedowm(user_story_5(ind_list, fam_list)[1])
+        writedowm(user_story_6(ind_list, fam_list)[1])
+        writedowm(user_story_7(ind_list)[1])
+        writedowm(user_story_8(ind_list, fam_list)[1])
+        writedowm(user_story_9(ind_list)[1])
+        writedowm(user_story_10(ind_list, fam_list)[1])
+        writedowm(user_story_11(ind_list, fam_list)[1])
+        writedowm(user_story_12(ind_list)[1])
+        writedowm(user_story_13(ind_list, fam_list)[1])
+        writedowm(user_story_14(ind_list, fam_list)[1])
+        writedowm(user_story_15(fam_list)[1])
+        writedowm(user_story_16(ind_list, fam_list)[1])
